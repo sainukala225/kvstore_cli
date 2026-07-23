8d94a6f31caa57e58db323c1072948ddb2f37b4f
sainukala <sainukala370@gmail.com>
2026-07-23 21:11:34 +0530

    add interface methods

--- include/kvstore.h
+++ include/kvstore.h
@@ -1,3 +1,12 @@
 #ifndef KVSTORE_H
 #define KVSTORE_H
+
+typedef struct kvstore_t *Kvstore;
+
+void *put_key(Kvstore store, char *key, char *value);
+void *get_key(Kvstore store, char *key);
+int delete_key(Kvstore store, char *key);
+void stats(Kvstore store);
+int save_to_file(Kvstore store, char *filepath);
+int load_from_file(Kvstore store, char *filepath);
 #endif
