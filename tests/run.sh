ae6f84a4fd6cadb6f40f28bc76efda80debb5412
sainukala <sainukala370@gmail.com>
2026-08-21 00:27:54 +0530

    add state tests

--- tests/run.sh
+++ tests/run.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 
 fail=0
-for t in cases_tests/cases.sh errors_tests/errors.sh parsing_tests/parsing.sh; do
+for t in cases_tests/cases.sh errors_tests/errors.sh parsing_tests/parsing.sh state_tests/state.sh; do
   "$t" || fail=1
 done
 
