8f08fdbacc63390ecc843dfe3b787b6084b0d215
sainukala <sainukala370@gmail.com>
2026-08-28 01:38:05 +0530

    change errors.sh to errors.py

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,7 @@ set -u
 cd "$(dirname "$0")"
 
 fail=0
-for t in cases_tests/cases.py errors_tests/errors.sh parsing_tests/parsing.sh \
+for t in cases_tests/cases.py errors_tests/errors.py parsing_tests/parsing.sh \
   state_tests/state.sh roundtrip_tests/roundtrip.sh \
   load_failure_tests/load_failure_tests.py; do
   "$t" || fail=1
